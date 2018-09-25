94f35085c9f4c33b6156ce4bf0c11eba856b7b81
Sameer Saini <sameer.saini@xirtah.com>
2018-09-25 15:22:59 +1000

    Move core from gopa to gopa-framework

---
+++ core/env/generated.go
@@ -0,0 +1,6 @@
+package env
+
+const lastCommitLog = "e58ce67, Tue Sep 25 10:36:49 2018 +1000, Sameer Saini, Add custom notes file for remebering how to set everything up again "
+const buildDate = "Tue Sep 25 12:42:28 AEST 2018"
+
+const version  = "0.11.0_SNAPSHOT"
